72b57d8dd2cc9c9e2ca6845cb7952a423d2fc8ba
NotAddison <S10222525@connect.np.edu.sg>
2022-05-16 03:05:42 +0800

    Init Files: -Uploaded files from wk 3 practical -Added function to create 20 User objects

---
+++ app/src/main/java/sg/edu/np/mad/madpractical/ListActivity.java
@@ -0,0 +1,89 @@
+package sg.edu.np.mad.madpractical;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ListActivity extends AppCompatActivity {
+    private static final String TAG = "ListActivity";
+    private ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+
+
+        // -- Get Elements
+        img = (ImageView) findViewById(R.id.imageLogo);
+
+        // On click
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.v(TAG, "Clicked on the image");
+
+                // Defining Content in Alert Dialogue
+                AlertDialog.Builder builder = new AlertDialog.Builder( ListActivity.this);
+                builder.setTitle("Profile");
+                builder.setMessage("MADNess");
+                builder.setCancelable(false);
+                builder.setPositiveButton("View", new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
+                        Log.v(TAG, "Clicked on View");
+                        // --- Generating Random Number ---
+                        Random random = new Random();
+                        int randomNumber = random.nextInt();
+                        while (randomNumber < 0){               // Prevent Negative Numbers
+                            randomNumber = random.nextInt();
+                        }
+                        Log.v(TAG, "randomNumber: " + randomNumber);
+
+                        Intent intent = new Intent(ListActivity.this, MainActivity.class);
+                        intent.putExtra("Number", String.valueOf(randomNumber));  // Must pass a string value (Not Int)
+                        startActivity(intent);
+                    }
+                });
+                builder.setNegativeButton("Close", new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
+                        Log.v(TAG, "Clicked on Close");
+                    }
+                });
+
+                AlertDialog alert = builder.create();   // Create the Alert Dialogue
+                alert.show();                           // Show the Alert Dialogue
+            }
+        });
+
+        // --- Generate 20 Random Users  ---
+        Random rand = new Random();
+        ArrayList<Boolean> boolList= new ArrayList<Boolean>();
+        boolList.add(true);
+        boolList.add(false);
+
+        ArrayList<User> userList= new ArrayList<User>();
+
+        for(int i=0; i< 20; i++){
+            String name = String.valueOf(rand.nextInt(100000));
+            String description = String.valueOf(rand.nextInt(100000));
+            boolean followed = boolList.get(rand.nextInt(2));
+
+            User user =  new User(name,description,i,followed);
+            userList.add(user);
+            // Log.v(TAG, "Bool :"+ followed + " Name :"+ user.name);
+        }
+
+
+
+    }
+}
---
+++ app/src/main/java/sg/edu/np/mad/madpractical/User.java
@@ -0,0 +1,15 @@
+package sg.edu.np.mad.madpractical;
+
+public class User {
+    public String name;
+    public String description;
+    public int id;
+    public boolean followed;
+
+    public User(String Name, String Description, int Id, boolean Followed) {
+        name = Name;
+        description = Description;
+        id = Id;
+        followed = Followed;
+    }
+}
